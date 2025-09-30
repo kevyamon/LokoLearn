5d71794ef96fbc5d2493a11fcd719274c40ac308
baticlean <baticlean225@gmail.com>
2025-09-30 00:33:11 +0000

    xx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ModalProvider } from './contexts/ModalContext.jsx';
-import { SearchProvider } from './contexts/SearchContext.jsx'; // 1. Importer
+import { SearchProvider } from './contexts/SearchContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SearchProvider> {/* 2. Envelopper */}
+    <SearchProvider>
       <ModalProvider>
         <App />
       </ModalProvider>
     </SearchProvider>
   </React.StrictMode>,
 );
+
+// On ajoute ce script à la fin
+const loader = document.getElementById('loader-wrapper');
+if (loader) {
+  // On attend un peu pour que l'effet ne soit pas trop rapide
+  setTimeout(() => {
+    loader.classList.add('fade-out');
+    // On supprime complètement l'élément après la transition pour nettoyer
+    loader.addEventListener('transitionend', () => loader.remove());
+  }, 1000); // Garde le loader visible pendant au moins 1 seconde
+}
